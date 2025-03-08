804490f791ea2d026f6cc36754368ef6f3614269
Deepak Barkhane <144546324+deepakbarkhane1610@users.noreply.github.com>
2025-03-08 18:32:38 +0000

    let, var and const

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
 console.log("Deepak Barkhane")
+
+
+const accountId = 201025
+
+let stuName = "Deepak Barkhane"
+
+var subject = "CSE"
+
+accountId = 222222
+
+stuName = "Avinesh"
+
+subject = "Electrician"
+
+console.log(accountId)
+
+console.log(stuName)
+
+console.log(subject)
